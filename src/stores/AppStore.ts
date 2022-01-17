8e345439a2cef3aa5ac487482be20a1cb0eb56b3
aimen <artem.igorevich8@rambler.ru>
2022-01-17 19:09:35 +0300

    setup mobx

---
+++ src/stores/AppStore.ts
@@ -0,0 +1,10 @@
+import { makeAutoObservable } from "mobx";
+
+const AppStore = () => makeAutoObservable({
+  searchQuery: "",
+  handleSearchQueryChange(value: string) {
+    this.searchQuery = value;
+  }
+});
+
+export default AppStore;
